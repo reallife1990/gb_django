4b20401f2dd2b519985b3d96f20dd05d92591a97
Pavel <reallife1990msk@mail.ru>
2022-10-21 17:33:49 +0300

    les_02

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.http import HttpResponse
-
+from datetime import datetime
 
 class IndexView(TemplateView):
     template_name = 'mainapp/index.html'
@@ -9,6 +9,33 @@ class IndexView(TemplateView):
 
 class ContactsView(TemplateView):
     template_name = 'mainapp/contacts.html'
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data['contacts'] = [
+            {
+                'map': "https://yandex.ru/map-widget/v1/-/CCUAZHcrhA",
+                'city': 'Санкт‑Петербург',
+                'phone':'+7-999-11-11111',
+                'email': 'geeklab@spb.ru',
+                'address':'территория Петропавловская крепость, 3Ж'
+            },
+            {
+                'map': 'https://yandex.ru/map-widget/v1/-/CCUAZHX3xB',
+                'city': 'Казань',
+                'phone': '+7-999-22-22222',
+                'email': 'geeklab@kz.ru',
+                'address': 'территория Кремль, 11, Казань, Республика Татарстан, Россия'
+            },{
+                'map': 'https://yandex.ru/map-widget/v1/-/CCUAZHh9kD',
+                'city': 'Москва',
+                'phone':'+7-999-33-3333',
+                'email': 'geeklab@msk.ru',
+                'address':'Красная площадь, 7, Москва, Россия'
+            },
+        ]
+
+        return context_data
 
 
 class CourseslistView(TemplateView):
@@ -25,4 +52,38 @@ class LoginView(TemplateView):
 
 class NewsView(TemplateView):
     template_name = 'mainapp/news.html'
+
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data['news_list'] = [
+            {
+                'title':'Заголовок 1',
+                'context':'Содержание 1',
+                'date': datetime.now(),
+            },
+            {
+                'title': 'Заголовок 2',
+                'context': 'Содержание 2',
+                'date': datetime.now(),
+            },
+            {
+                'title': 'Заголовок 3',
+                'context': 'Содержание 3',
+                'date': datetime.now(),
+            },
+            {
+                'title': 'Заголовок 4',
+                'context': 'Содержание 4',
+                'date': datetime.now(),
+            },
+            {
+                'title': 'Заголовок 5',
+                'context': 'Содержание 5',
+                'date': datetime.now(),
+            },
+            ]
+
+        return context_data
+
 
